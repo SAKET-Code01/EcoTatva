fc9fc43a7e415015678ff76eb41a268c78c157a2
saketkumar16102006-code <saketkumar16102006@gmail.com>
2026-04-27 02:02:49 +0530

    Temporary fix - put Firebase keys directly in code

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDR8oPidJfmuqkkqDD20PBlz2UgPJSJg2A",
+  authDomain: "ecotatva-efa31.firebaseapp.com",
+  projectId: "ecotatva-efa31",
+  storageBucket: "ecotatva-efa31.firebasestorage.app",
+  messagingSenderId: "946743225357",
+  appId: "1:946743225357:web:f1d5026533686d1bf97944",
 };
 
 const app = initializeApp(firebaseConfig);
